195bb88d34d8ee5e05b6d94d8c69b2b7dd52b6e6
injulme <injulme0309@naver.com>
2023-12-26 22:21:21 +0900

    feat: chat ui

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,7 @@
+import "./App.css";
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
+import dayjs from "dayjs";
 
 const socket = io("http://localhost:3001", {
   transports: ["websocket"],
@@ -31,33 +33,61 @@ function App() {
   }, []);
 
   const sendMessage = () => {
+    if (!inputMessage) return;
     const data = {
       user: user,
       msg: inputMessage,
+      datetime: new Date(),
     };
     socket.emit("message", data);
     setInputMessage("");
   };
+  const enterKeyPress = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      sendMessage();
+    }
+  };
 
   return (
-    <div>
-      {connected ? `접속완료! 님이 들어왔습니다.` : "접속중..."}
-      <ul>
-        {messages.map((message, index) => (
-          <li
-            key={index}
-            style={{ color: message.user === user ? "blue" : "pink" }}
-          >
-            {message.user}:{message.msg}
-          </li>
-        ))}
-      </ul>
-      <input
-        type="text"
-        value={inputMessage}
-        onChange={(e) => setInputMessage(e.target.value)}
-      />
-      <button onClick={sendMessage}>전송</button>
+    <div className="container">
+      {connected ? `접속완료!` : "접속중..."}
+
+      <div className="container__message">
+        {messages.map((message, index) => {
+          const isMe = message.user === user;
+          const datetime = dayjs(message.datetime).format("h:mm A");
+          return (
+            <div key={index} className="box__message">
+              <div
+                className={`message ${isMe ? "message-me" : "message-other"}`}
+              >
+                {message.msg}
+              </div>
+              <span
+                className={`message__datetime ${
+                  isMe ? "datetime-me" : "datetime-other"
+                }`}
+              >
+                {datetime}
+              </span>
+            </div>
+          );
+        })}
+      </div>
+      <div className="container__send">
+        <input
+          type="text"
+          value={inputMessage}
+          onChange={(e) => setInputMessage(e.target.value)}
+          className="send__input"
+          placeholder="메세지를 입력하세요."
+          onKeyDown={enterKeyPress}
+        />
+        <button onClick={sendMessage} className="send__button">
+          전송
+        </button>
+      </div>
     </div>
   );
 }
